fc746099a313765b7e6821cf1e7f046d78d98a0c
Marco Gutierrez <a17001568@idat.pe>
2026-08-04 00:55:50 -0500

    Iniciando la estructura de la  Biblioteca Microservices

---
+++ SERVICES/ms-multas/src/main/java/pe/edu/idat/ms_multas/MsMultasApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.ms_multas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMultasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMultasApplication.class, args);
+	}
+
+}
---
+++ SERVICES/ms-multas/src/test/java/pe/edu/idat/ms_multas/MsMultasApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.ms_multas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMultasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
